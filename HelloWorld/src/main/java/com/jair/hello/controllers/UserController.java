8f73930440a442d1f1e5aa9f9b99b9c317bea021
Jair Axel Lizarraga Velarde <jairlizarraga@ibm.com>
2022-04-11 19:13:35 -0500

    create and update User fields

--- HelloWorld/src/main/java/com/jair/hello/controllers/UserController.java
+++ HelloWorld/src/main/java/com/jair/hello/controllers/UserController.java
@@ -17,17 +17,33 @@ public class UserController {
     @Autowired
     UserRepository userRespository;
 
-    @GetMapping("/user")
+    @GetMapping("/api/user")
     public List<User> index(){
         return userRespository.findAll();
     }
 
-    @GetMapping("/user/{id}")
+    @GetMapping("/api/user/{id}")
     public Optional<User> show(@PathVariable String id){
         int userId = Integer.parseInt(id);
         return userRespository.findById(userId);
     }
+    
+    @PostMapping(value="/api/user/")
+    public User create(@RequestBody Map<String, String> body){
+        String name = body.get("name");
+        String password = body.get("password");
+        return userRespository.save(new User(name, password));
+    }
 
+    @PutMapping("/api/user/{id}")
+    public User update(@PathVariable String id, @RequestBody Map<String, String> body){
+        int userId = Integer.parseInt(id);
 
+        User user = userRespository.findById(userId).get();
+        user.setName(body.get("name"));
+        user.setPassword(body.get("password"));
+        return userRespository.save(user);
+    }
+    
 
 }
--- HelloWorld/src/main/java/com/jair/hello/models/User.java
+++ HelloWorld/src/main/java/com/jair/hello/models/User.java
@@ -18,6 +18,17 @@ public class User {
 
     public User() {  }
 
+    public User(int id, String name, String password) { 
+    	this.id = id;
+    	this.name = name;
+    	this.password = password;
+    }
+
+    public User(String name, String password) { 
+    	this.name = name;
+    	this.password = password;
+    }
+
 	public int getId() {
 		return id;
 	}
